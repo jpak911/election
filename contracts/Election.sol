0ca2e575bb5c57bbe0625c1d3433d273d6c4403c
jpak911 <jpak911@gmail.com>
2022-01-09 12:21:52 -0500

    Add files via upload

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.16;
+
+contract Election {
+	//Store Candidate
+	//Read candidate
+	string public candidate;
+	//Constructor
+	function Elections () public {
+		candidate = "Candidate 1";
+	}
+}
